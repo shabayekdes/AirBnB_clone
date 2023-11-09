21a01c74fc76b6448b5d2e11b01cf71fc74fd8dc
MaryomaElsayed <maryamelsayd.1@gmail.com>
2023-11-08 21:04:41 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from datetime import datetime
 from uuid import uuid4
-#import models
+import models
 
 class BaseModel:
     def __init__(self , *arguments , **KeyWordArguments):
