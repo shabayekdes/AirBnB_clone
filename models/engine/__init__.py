ebc0e0b5c43bab8211493a72d4cd28d3c21bcbd7
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-11-10 21:01:33 +0200

    Add file storage class

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-""" __init__ create a unique FileStorage
-instance for the application
-"""
+#!/usr/bin/env python3
+"""__init__ magic method"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
