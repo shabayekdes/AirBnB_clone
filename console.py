b7b1e200e7cecb7ff750ce9f9e56be88a6f1d593
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-11-10 23:02:20 +0200

    Update console and implement do_create instance method

--- console.py
+++ console.py
@@ -1,56 +1,84 @@
 #!/usr/bin/env python3
-"""
-Console for object management and storage
+""" Main script for the HBnB console.
 """
 import cmd
+import shlex
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HBnB command interpreter.
-    Attributes:
-        prompt (str): The command prompt.
+    """ HBnBCommand class defines a command-line 
+    interpreter for object management and storage.
     """
+
+    my_models = {"BaseModel": BaseModel, "User": User, "State": State,
+              "City": City, "Amenity": Amenity,
+              "Place": Place, "Review": Review}
 
     prompt = "(hbnb) "
 
     def do_quit(self, args):
-        """Quit command to exit the program.\n"""
+        """ Quit command to exit the program.\n
+        """
         quit()
 
     def do_EOF(self, args):
-        """End Of File command to exit the program"""
+        """ End Of File command to exit the program.\n
+        """
         quit()
 
     def emptyline(self):
-        """Do nothing on empty line\n"""
-        pass
-
-    def do_create(self, args):
-        """Create a new instance"""
-        print("Create ...!")
-        pass
-
-    def do_show(self, args):
-        """Prints the string representation of an instance."""
-        print("Show ...!")
-        pass
-
-    def do_destroy(self, args):
-        """Deletes an instance."""
-        print("Delete ...!")
+        """ Do nothing on empty line\n
+        """
         pass
 
     def do_all(self, args):
-        """Prints all string representation of all instances"""
+        """ Print string representation of all instances.
+        """
         print("Get All ...!")
         pass
 
+    def do_show(self, args):
+        """ Print the string representation of an instance.
+        """
+        print("Show ...!")
+        pass
+
+    def do_create(self, args):
+        """ Create a new instance of a specified class.
+        """
+        args = shlex.split(args)
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in HBNBCommand.my_models:
+            print("** class doesn't exist **")
+            return
+        else:
+            new_object = eval(args[0])()
+            new_object.save()
+            print(new_object.id)
+
     def do_update(self, args):
-        """Updates an instance based on the class name."""
+        """ Update an instance based on the class name.
+        """
         print("Update ...!")
         pass
 
+    def do_destroy(self, args):
+        """ Delete an instance.
+        """
+        print("Delete ...!")
+        pass
+
     def do_count(self, args):
-        """Counts the number of instances of a class"""
+        """ Count the number of instances of a class.
+        """
         print("Count ...!")
         pass
 
